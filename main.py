c7a324fcd76fe09b09baac8288217a91c74956f0
lcj <1150267318@qq.com>
2022-03-15 16:51:59 +0800

    feat:修改md

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import os
 
 from github import Github
 
-MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
+MD_HEAD = """**<p align="center">[lcj's Blog](https://liu-cj25.github.io/)</p>**
 ====
 
 **<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
-[![](https://user-images.githubusercontent.com/22115219/149507985-22e22223-4644-47bf-abf9-c388c287cc38.JPEG)](https://blog.leeyom.top)
-
 ## 联系方式
 - Phone：13046255574
 """
